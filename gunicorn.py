e71ab429e006c6135b8b854f29748ea45fd55372
Yuri <yurifc4@gmail.com>
2023-08-07 21:42:29 -0300

    workers

--- gunicorn.py
+++ gunicorn.py
@@ -4,12 +4,12 @@ from config.migrations.main import create_tables
 
 cpus = multiprocessing.cpu_count()
 
-bind = '0.0.0.0:8000'
-workers = int(cpus / 2) + 1
+bind='0.0.0.0:8000'
+workers=1
 threads=cpus * 2
-wsgi_app = 'src.app:wsgi'
+wsgi_app='src.app:wsgi'
 worker_class='eventlet'
-loglevel = 'info'
+loglevel='info'
 chdir=os.path.dirname(os.path.abspath(__file__))
 timeout=300
 limit_request_line=8188
